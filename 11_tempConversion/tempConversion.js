19371f3e9bc658efff922491bb8efa9ba9793588
DPnimo11 <dpnimo11@gmail.com>
2025-06-23 14:15:58 -0400

    more javascript exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var ans = (temp - 32) * 5 / 9;
+  return  Number.isInteger(ans) ? ans :  + ans.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var ans = temp * 9 / 5 + 32;
+  return  Number.isInteger(ans) ? ans : + ans.toFixed(1);
 };
 
 // Do not edit below this line
